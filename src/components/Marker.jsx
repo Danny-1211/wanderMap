08d1e0c93378b3d1cceb399198d7a7e6e23fcb04
Danny-1211 <danny21316@gmail.com>
2025-08-07 17:22:44 +0800

    修改 popup 樣式

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -1,32 +1,64 @@
-import '../assets/utils/marker.scss'
-export function Marker(place) {
+import '../assets/utils/marker.scss';
+export function Marker(place, mapContainerEl) {
   const el = document.createElement('div');
   el.className = 'marker';
 
   const popup = document.createElement('div');
-  popup.className = 'popup';
-  popup.innerHTML = `
+  popup.className = 'pop-noShow';
+
+
+  // el.appendChild(popup);
+  mapContainerEl.appendChild(popup);
+
+
+  el.addEventListener("mouseenter", function (e) {
+    popup.innerHTML = `
     <div class="popup-content">
       <div class="popup-image">
-        <img src="" alt="" />
+        <img src=${place.pic} alt="" />
       </div>
       <div class="popup-info">
-        <p></p>
+        <div class="info-title"> <p>${place.title}</p></div>     
+        <div class="info-date"><p>${place.date}</p></div>
+        <div class="info-desc"><p>${place.description}</p></div>
       </div>
     </div>
     <div class="popup-tip"></div>
   `;
+    // 定位 popup 位置（可加動畫、偏移）
+    const rect = el.getBoundingClientRect();
+    popup.style.position = 'absolute';
+    popup.style.left = `${rect.left + window.scrollX}px`;
+    popup.style.top = `${rect.top + window.scrollY}px`; // 視覺上浮在 marker 上方
+    popup.style.zIndex = 1000;
+    removeClass(popup, 'pop-noShow');
+  });
 
-  el.appendChild(popup);
+  el.addEventListener("mouseleave ", function (e) {
+    addClass(popup, 'pop-noShow');
+    removeClass(popup, 'pop-show');
+  });
+
+    // 當滑鼠移到 popup 上時也不消失
+  popup.addEventListener("mouseenter", function () {
+    removeClass(popup, 'pop-noShow');
+  });
+  popup.addEventListener("mouseleave", function () {
+    addClass(popup, 'pop-noShow');
+    removeClass(popup, 'pop-show');
+  });
+  
   return el;
 }
 
+
+
 function addClass(element, name) {
-    element.classList.add(name);
+  element.classList.add(name);
 }
 
 function removeClass(element, name) {
-    element.classList.remove(name);
+  element.classList.remove(name);
 }
 
 
